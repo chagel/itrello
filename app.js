d82b887a4bb31446838c8f73a3d55bd3066d634b
MGC <chagel@gmail.com>
2020-04-09 16:47:15 -0700

    v1.2

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
-console.debug("Hide boards")
-
-board = document.getElementById("board")
-
-var gettingItem = browser.storage.sync.get('boards');
-gettingItem.then((res) => {
-
-  boards = res.boards
-  if (boards == undefined) {
-    boards = 'Staging|Backlog|Doing|Testing'
+function _toggleElementsByClass(className) {
+  els = document.getElementsByClassName(className)
+  for (let i=0; i < els.length; i++) {
+    els[i].style.display = 'none'
   }
+}
 
-  let re = new RegExp(boards, 'g')
-  for (let i=0; i < board.children.length; i++) {
-    list = board.children[i]
-    title = list.firstElementChild.firstElementChild.firstElementChild.nextElementSibling
-    if(title && !title.innerText.match(re)) {
-      list.style.display = 'none'
+function toggleBoards() {
+  console.debug("Hide boards")
+  board = document.getElementById("board")
+
+  var gettingItem = browser.storage.sync.get('boards');
+  gettingItem.then((res) => {
+
+    boards = res.boards
+    if (boards == undefined) {
+      boards = 'Staging|Backlog|Doing|Testing'
     }
+
+    let re = new RegExp(boards, 'g')
+    for (let i=0; i < board.children.length; i++) {
+      list = board.children[i]
+      title = list.firstElementChild.firstElementChild.firstElementChild.nextElementSibling
+      if(title && !title.innerText.match(re)) {
+        list.style.display = 'none'
+      }
+    }
+  })
+}
+
+function compactCards() {
+  console.debug("Compact cards")
+  _toggleElementsByClass("list-card-cover")
+}
+
+function toggleBadges() {
+  console.debug("Hide badges")
+  _toggleElementsByClass("badges")
+}
+
+function showCardIds() {
+  console.debug("Show card id")
+  ids = document.getElementsByClassName("card-short-id")
+  for (let i=0; i < ids.length; i++) {
+    ids[i].classList.remove('hide')
+    ids[i].style.fontWeight = '600'
   }
-})
-
-
-console.debug("Compact cards")
-cards = document.getElementsByClassName("list-card-cover")
-for (let i=0; i < cards.length; i++) {
-  cards[i].style.display = 'none'
 }
 
-console.debug("Hide badges")
-badges = document.getElementsByClassName("badges")
-for (let i=0; i < badges.length; i++) {
-  badges[i].style.display = 'none'
+function toggleStickers() {
+  console.debug("Toggle stickers")
+  _toggleElementsByClass("list-card-stickers-area")
 }
 
-console.debug("Show card id")
-ids = document.getElementsByClassName("card-short-id")
-for (let i=0; i < ids.length; i++) {
-  ids[i].classList.remove('hide')
-  ids[i].style.fontWeight = '600'
-}
+toggleBoards()
+compactCards()
+toggleBadges()
+showCardIds()
+toggleStickers()
 
